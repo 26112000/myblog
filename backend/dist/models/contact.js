b08b71fc1316cb4383896607ca4b86de20c371c5
26112000 <accoffshore1@gmail.com>
2024-06-19 20:43:06 +0300

    myblog

---
+++ backend/dist/models/contact.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.findAll = void 0;
+const db_1 = require("../db");
+const findAll = (callback) => {
+    const queryString = `SELECT * FROM contact`;
+    db_1.db.query(queryString, (err, result) => {
+        if (err) {
+            callback(err);
+        }
+        const rows = result;
+        const contact_messages = [];
+        rows.forEach((row) => {
+            const contact_message = {
+                id: row.id,
+                nume: row.nume,
+                prenume: row.prenume,
+                email: row.email,
+                mesaj: row.mesaj,
+            };
+            contact_messages.push(contact_message);
+        });
+        callback(null, contact_messages);
+    });
+};
+exports.findAll = findAll;
